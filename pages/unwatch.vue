a7f17c4e08237af7f2dabcc94f19e8b7cba19df8
Howard <howard.tzw@gmail.com>
2023-06-28 16:41:06 +0800

    update

---
+++ pages/unwatch.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const count = ref(0)
+
+const unwatch = watch(count, (newVal, oldVal) => {
+	console.log('watch', newVal, oldVal)
+	if (count.value === 5) {
+		unwatch()
+	}
+})
+</script>
+
+<template>
+	<div>
+		<p>{{ count }}</p>
+		<button class="btn" @click="count++">Increment</button>
+	</div>
+</template>
